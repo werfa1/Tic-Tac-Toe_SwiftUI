2194774f56abd0d24c886f7f6091688d3acf06db
werfa1 <pasha.otverchenko@gmail.com>
2021-09-01 15:41:46 +0300

    Added computer moves imitation

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -7,27 +7,40 @@
 
 import SwiftUI
 
-let columns : [GridItem] = [GridItem(.flexible()),
-                            GridItem(.flexible()),
-                            GridItem(.flexible())]
-
 struct ContentView: View {
+    
+    let columns : [GridItem] = [GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
                 LazyVGrid(columns: columns, spacing: 5){
-                    ForEach(0..<9) {_ in
+                    ForEach(0..<9) {i in
                         ZStack {
                             Circle()
                                 .foregroundColor(.purple)
                                 .frame(width: geometry.size.width / 3 - 15,
                                        height: geometry.size.width / 3 - 15)
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40,
                                        height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            guard moves[i] == nil else { return }
+                            moves[i] = Move(player: .human,
+                                            boardIndex: i)
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                            }
                         }
                     }
                 }
@@ -35,6 +48,32 @@ struct ContentView: View {
             }
             .padding()
         }
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index})
+    }
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        var movePosition = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(in: moves, forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        return movePosition
+    }
+}
+
+enum Player {
+    case human
+    case computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
