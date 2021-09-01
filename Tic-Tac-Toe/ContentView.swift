263ae80dbe25d25037b5d1d2da9b200e0a04f91d
werfa1 <pasha.otverchenko@gmail.com>
2021-09-01 14:10:49 +0300

    Created game board prototype

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+let columns : [GridItem] = [GridItem(.flexible()),
+                            GridItem(.flexible()),
+                            GridItem(.flexible())]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, fella")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5){
+                    ForEach(0..<9) {_ in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.purple)
+                                .frame(width: geometry.size.width / 3 - 15,
+                                       height: geometry.size.width / 3 - 15)
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40,
+                                       height: 40)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                Spacer()
+            }
             .padding()
+        }
     }
 }
 
