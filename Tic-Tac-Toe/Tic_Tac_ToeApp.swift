f43cfb766ac3166805534b0e85ca9a4bb82e846b
werfa1 <pasha.otverchenko@gmail.com>
2021-09-01 13:46:30 +0300

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Pavel Otverchenko on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
